81b71ed743b86a1fbac5173908e9354a7f057ade
Deepak Yadav <yadavpankaj01011992@gmail.com>
2023-01-15 00:17:55 +0530

    All code

---
+++ WebApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication.Models;
+
+namespace WebApplication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Students> Students { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-F121RJT\MSSQLSERVER01;Database=test;User id=sa;Password=$epl@1234;Integrated Security=false;MultipleActiveResultSets=true");
+        }
+    }
+}
---
+++ WebApplication/Models/Students.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class Students
+    {
+        public int Id { get; set; } // this represents our primary key in the table that we're going to migrate.
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Section { get; set; }
+    }
+}
